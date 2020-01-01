5951149983f8dd4935857f5b2372295d3c671462
AnKiWoong <ankiwoong@gmail.com>
2020-01-01 09:26:05 +0900

    master

---
+++ Basic7_String/1754_11800964(AC).py
@@ -0,0 +1,10 @@
+﻿import io, sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
+
+n1, n2 = map(int, input().split())
+
+if n1 > n2:
+    print(n2, n1)
+else:
+    print(n1, n2)
+
