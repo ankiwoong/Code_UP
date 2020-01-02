acfcd3508770eb44a7c57a1cf652cf187d5698c4
AnKiWoong <ankiwoong@gmail.com>
2020-01-02 21:14:36 +0900

    master

---
+++ Basic8_2_D_Array/1476_11814164(AC).py
@@ -0,0 +1,18 @@
+﻿import io, sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
+
+n, m = map(int, input().split())
+matrix = [[0]*m for i in range(n)]
+cnt = 0
+
+for i in range(0, n+m-1):
+    for j in range(0, m):
+        for k in range(0, n):
+            if j+k == i:
+                cnt += 1
+                matrix[k][j] = cnt
+
+for i in range(0, n):
+    for j in range(0, m):
+        print(matrix[i][j], end=' ')
+    print()
